5966527c5613559829c73c4115f0309c40f420c3
songJ1128 <jerrysong1128@gmail.com>
2024-11-19 22:28:38 -0500

    added function to get a process starting at an address

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include <tlhelp32.h>
 #include <iostream>
 #include <vector>
-
+#include <psapi.h>
 #define TH32CS_PROCESS 0x00000002
 
 
@@ -39,7 +39,7 @@ std::vector<ProcessInfo> getProcessList() {
 
             HANDLE hProcess = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, FALSE, info.processID);
             if (hProcess) {
-                PROCESS_MEMORY_COUNTERS pmc;
+                PROCESS_MEMORY_COUNTERS_EX pmc;
                 if (GetProcessMemoryInfo(hProcess, &pmc, sizeof(pmc))) {
                     info.workingSetSize = pmc.WorkingSetSize;
                     processes.push_back(info);
@@ -51,4 +51,31 @@ std::vector<ProcessInfo> getProcessList() {
 
     CloseHandle(hSnapshot);
     return processes;
+}
+
+void getMemoryRegions(ProcessInfo& processInfo, HANDLE hProcess) {
+    MEMORY_BASIC_INFORMATION mbi;
+    BYTE* address = nullptr;
+
+    while (VirtualQueryEx(hProcess, address, &mbi, sizeof(mbi)) == sizeof(mbi)) {
+        MemoryRegion region;
+        region.baseAddress = mbi.BaseAddress;
+        region.regionSize = mbi.RegionSize;
+        region.state = mbi.State;
+        processInfo.memoryRegions.push_back(region);
+        address += mbi.RegionSize;
+    }
+}
+
+int main() {
+    std::vector<ProcessInfo> processes = getProcessList();
+
+    for (const auto& process : processes) {
+        std::cout << "Process Name: " << process.processName << ", Process ID: " << process.processID << ", Working Set Size: " << process.workingSetSize << " bytes" << std::endl;
+        for (const auto& region : process.memoryRegions) {
+            std::cout << "  Base Address: " << region.baseAddress << ", Region Size: " << region.regionSize << " bytes, State: " << region.state << std::endl;
+        }
+    }
+
+    return 0;
 }
